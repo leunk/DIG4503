7aa540f9f752d518c3c38455d92a71bc8600ca47
leunk <43124250+leunk@users.noreply.github.com>
2020-01-17 17:34:35 -0500

    Delete Katherine.js

--- Katherine.js
+++
@@ -1,9 +0,0 @@
-const Person = require("./Person.js");
-
-class Katherine extends Person{
-    constructor(name, favoriteColor, age){
-        super(name, favoriteColor);
-        this.age = age;
-    }
-}
-module.exports = Katherine;
