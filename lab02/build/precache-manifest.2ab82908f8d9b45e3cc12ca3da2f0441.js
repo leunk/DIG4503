df9231ea44df296fd937ba368810ef260db493ca
leunk <43124250+leunk@users.noreply.github.com>
2020-01-23 20:42:07 -0500

    Add files via upload

---
+++ lab02/build/precache-manifest.2ab82908f8d9b45e3cc12ca3da2f0441.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e04f54711cdecbbe4eeaaf28e9433d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe023a1a65b858720c90",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "abea21909b57663f640f",
+    "url": "/static/js/2.354affd3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.354affd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe023a1a65b858720c90",
+    "url": "/static/js/main.e7a93ef7.chunk.js"
+  },
+  {
+    "revision": "8cf91d962beffed7bd69",
+    "url": "/static/js/runtime-main.bc31fc83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
