a04398301f0f6ce4e8cb4babb2730610ebfbb653
leunk <leunk@knights.ucf.edu>
2020-01-30 20:18:34 -0500

    lab3

---
+++ lab3/index.js
@@ -0,0 +1,16 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+
+App.use("/public", Express.static("public"));
+
+App.get("/", function(req, res){
+    res.send("Hello World!");
+});
+
+
+App.listen(port, () => {
+    console.log("Server running!");
+});
+
