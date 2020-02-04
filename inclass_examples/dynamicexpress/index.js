0cdb5d2de20889f9b3397fc139f902234e0e8fed
leunk <leunk@knights.ucf.edu>
2020-02-03 19:06:55 -0500

    wk5 - monday

--- inclass_examples/dynamicexpress/index.js
+++ inclass_examples/dynamicexpress/index.js
@@ -1,11 +1,43 @@
 const Express = require("express");
 const App = Express();
-const port = 80;
+const port = 420;
 
-App.get("/:book/:id", (req,res) => {
-    res.send(req.params);
+const Person = require("./Person.js");
+let p = new Person("Kat", "purple");
+//new - create an object based on a class
+let people = [];
+var faker = require('faker');
+let name = faker.name.findName();
+let color = faker.commerce.color();
+
+for(let i = 0; i < 99; i++){
+   let name = faker.name.findName();
+   let color = faker.commerce.color();
+    people.push(new Person(name, color));
+}
+
+App.get("/person/name/:name", (req,res) => {
+    //res.send(req.params.name);
+    //search an array to find a particular name how to?
+    people.forEach((value) => {
+        if(value.name == req.params.name){
+            res.send(value);
+            //look thru array
+        }
+    });
+    res.send("Hey! Hey, you! No! No!");
+
+});
+
+App.get("/person/color/:color", (req,res) => {
+    //res.send(req.params.color);
+    people.forEach((value) => {
+        if(value.color == req.params.color){
+            res.send(value);
+        }
+    });
 });
 
 App.listen(port, () => {
     console.log("server running!");
-})
+});
