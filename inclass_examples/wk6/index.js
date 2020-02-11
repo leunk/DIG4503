ced1e980a4a6ba7824d33b2b2ba02d3dc3fb2438
Kat <43124250+leunk@users.noreply.github.com>
2020-02-10 19:05:00 -0500

    in class example

--- inclass_examples/wk6/index.js
+++ inclass_examples/wk6/index.js
@@ -2,11 +2,15 @@ const Express = require("express");
 const App = Express();
 const port = 80;
 
-App.use("/", Express.statis("public"));
+App.use("/", Express.static("public"));
 
-App.get("/api", (req,res) =>{
-    res.json({"error": "Error mess"})
+App.get("/api/:value", (req,res) =>{
+    let result = {"error": "Error mess"};
     //create object literal ({"error"});
+    if(req.params.value == 5){
+        result = {"secret": "you knew the secret"};
+    }
+    res.json(result)
 });
 
 App.listen(port, () => {
