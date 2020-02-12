c9111d1b8e59322a9d9fab90c7d9f76e3dc99ccc
leunk <leunk@knights.ucf.edu>
2020-02-12 17:46:44 -0500

    corrections to lab 5 - working on browser now

--- lab4/index.js
+++ lab4/index.js
@@ -13,13 +13,15 @@ const chalk = require("Chalk");
 //pokemon stuff 
 const pokemons = require("json-pokemon");
 //npmjs json-pokemon possible usage 
-const getPokemons = require("json-pokemon/getPokemon");
-const pokemonArray = pokemons;
+const getPokemons = require("json-pokemon"); //this will the array that is SEARCHING
+//to search array for loop , or forEach 
+
+// const pokemonArray = pokemons;
 
 //getPokemonByName -- returns list of pokemons by name
-const name = getPokemons.getPokemonByName();
+// const name = getPokemons.getPokemonByName(); no need for this because getPokemon will do job same with ID
 //getPokemonById -- returns pokemon by id
-const id = getPokemons.getPokemonById();
+// const id = getPokemons.getPokemonById();
 
 // App.use("/public", express.static("public")); 
 
@@ -27,44 +29,44 @@ const id = getPokemons.getPokemonById();
 //route for id
 App.get("/id/:id", (req, res)=>{
     let result = {"error": "Sorry, there were no matches"};
-    pokemonArray.forEach((value)=>{
-        if(value.id == req.params.id){
-             let index = value.id - 1;
+    getPokemons.forEach((value)=>{  
+        if(value.id == req.params.id){ 
+             let index = value.id - 1; //set result to what that is so that there is always a response 
              result = pokemonArray[index];
         }
     });
     //---------------some space---------------
     if(result.error){
-        console.log(chalk.red("There were no matches"));
+        console.log(chalk.red(req.path)); 
         //console.log(chalk.red("there were no matches"));
     }
     else{
         console.log(chalk.green(req.path));
         console.log(result);
     }
-    return result;
+    res.send(result); //r
 });
 
 //----------------------------------
 //route for name
 App.get("/name/:name", (req,res)=>{
     let result = {"error": "Sorry, there were no matches"};
-    pokemonArray.forEach((value) =>{
+    getPokemons.forEach((value) =>{
         if(value.name.toLowerCase() == req.params.name){
             let index = value.id - 1;
-            result = pokemonArray[index];
+            result = getPokemons[index];
         }
     });
     //---------------some space---------------
     if(result.error){
-    console.log(chalk.red("There were no matches"));
+    console.log(chalk.red(req.path));
     //console.log(chalk.red("there were no matches"));
     }
     else{
     console.log(chalk.green(req.path));
     console.log(result);
     }
-    return result;
+    res.send(result);
 });
 
 
