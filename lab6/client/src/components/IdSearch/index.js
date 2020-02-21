8fbbec14d391278fdc6b23c92624c0448c087113
leunk <leunk@knights.ucf.edu>
2020-02-21 13:43:11 -0500

    notes

--- lab6/client/src/components/IdSearch/index.js
+++ lab6/client/src/components/IdSearch/index.js
@@ -31,32 +31,8 @@ class IdSearch extends React.Component{
 }
 export default IdSearch;
 
-//this code didn't work 
-// import React from "react";
-
-// class IdSearch extends React.Component{
-
-//     readId(event){
-//         event.preventDefault();
-
-//         let pokeId = document.querySelector("#pokemonId");
-
-//         fetch("http://localhost:80/id/" + pokeId.value).then((res) => {
-//             return res.json();
-//         }).then((processed) =>{
-//             let pokeResponse = document.querySelector("#reportingArea");
-//             if(processed.error){
-//                 pokeResponse.innerHTML = processed.error;                        
-//             }
-//             else{
-//                 pokeResponse.innerHTML = processed.name;
-//             }
-//         });
-//     }
-//         render(){
-//             return(
-//             <div>
-//PROBLEM HERE: form needs to be set up <form> </form> if it is closed like so below it will break!
+//this part didn't work 
+//PROBLEM: form needs to be set up <form> </form> if it is closed like so below it will break!
 //                 <form onSubmit = {this.readId}/>
 //                 <p>Enter Pokemon ID:</p>
 //                 <input id= "pokemonId" type="text" />
