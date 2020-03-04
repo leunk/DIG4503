87a09e389d1d2ef3cbcb5d53ef134288c2fda025
leunk <leunk@knights.ucf.edu>
2020-03-04 13:47:22 -0500

    wk8 notes + review

--- inclass_examples/wk8/database.js
+++ inclass_examples/wk8/database.js
@@ -2,19 +2,52 @@ const MongoClient = require('mongodb').MongoClient; //from whatever object we ar
 
 const URL = "mongodb+srv://katherineleunglin:JqOeR9DexODxuT2T@cluster0-tc9sb.mongodb.net/";
 
-MongoClient.connect(URL, function(error, connection){
-    if(error){
-        throw error;
+class Database{
+    constructor(){
+        //define a value that will eventually be the collection
+        this.collection = null;
+        //define a value that will eventually be an object
+        this.connection = null;
+        //conect to the MongoDB system using the URL and using a callback function
+        MongoClient.connect(URL, (error, connection) => {
+            if(error){
+                //throw the error
+                //throwing an error stops the code!
+                throw error;
+            }
+            //save the connection details passed to the callback
+            //into something that can be accessed throughout the class
+            
+            this.connection = connection;
+
+            //from the connection, select database "DIG4503-78"
+            let database = connection.db("DIG4503-78");
+
+            //from the database, select a collection "movies"
+            this.collection = database.collection("Movies");
+        });
     }
 
-    let database = connection.db("DIG4503-78");
-    let collection = database.collection("Movies");
+findTitle(title){
+    if(this.collection != null) {
+        return this.collection.findOne({"title": title});
+    }
+}
+}
 
-    collection.findOne({
-        "score":79
-    }, {}, (error, result) =>{
-        console.log(result);
-    });
+// MongoClient.connect(URL, function(error, connection){
+//     if(error){
+//         throw error;
+//     }
 
-    connection.close();
-});
+//     let database = connection.db("DIG4503-78");
+//     let collection = database.collection("Movies");
+
+//     collection.findOne({
+//         "score":79
+//     }, {}, (error, result) =>{
+//         console.log(result);
+//     });
+
+//     connection.close();
+// });
--- inclass_examples/wk8/index.js
+++ inclass_examples/wk8/index.js
@@ -1,16 +1,49 @@
+//Require Express
+const Express = require('express');
+//create an app based on a single instance of Express
+const App = Express();
+//save the port number to listen
+const port = 80;
+//require database
+const Database = require("./Database.js");
+
+let database = new Database();
+
 const MongoClient = require('mongodb').MongoClient; //from whatever object we are requiring just give me mongo
 
-const URL = "mongodb+srv://katherineleunglin:JqOeR9DexODxuT2T@cluster0-tc9sb.mongodb.net/";
+App.get("movies/title/:title", (req, res) => {
+    //set an error
+    let result = {"error": "could not find!"};
 
-MongoClient.connect(URL, function(error, connection){
-    if(error){
-        throw error;
-    }
-
-    let database = connection.db("DIG4503-78");
-    let collection = database.collection("Movies");
-
-    console.log(collection);
-
-    connection.close();
+    //findTitle() will return a Primise
+    //once it finishes, send the response
+    database.findTitle(req.params.title).then((movie) => {
+        //if a movie was not found, it will be null
+        if(movie !=null){
+            //if it is not null, set result to whatever movie is 
+            result = movie;
+        }
+        //send as a response: either the "error" object or the movie object found
+        res.json(result);
+    });
 });
+
+//listen to port
+App.listen(port, () =>{
+    console.log("Server is runningggggg")
+});
+
+// const URL = "mongodb+srv://katherineleunglin:JqOeR9DexODxuT2T@cluster0-tc9sb.mongodb.net/";
+
+// MongoClient.connect(URL, function(error, connection){
+//     if(error){
+//         throw error;
+//     }
+
+//     let database = connection.db("DIG4503-78");
+//     let collection = database.collection("Movies");
+
+//     console.log(collection);
+
+//     connection.close();
+// });
