aa447913469397950188384f2a306e35ff7fdd86
leunk <leunk@knights.ucf.edu>
2020-03-19 17:52:14 -0400

    moved stuff around / still not working

--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -1,78 +1,76 @@
 //trying example from repl
 
-import NameSearch from "../components/NameSearch";
-import IdSearch from "../components/IdSearch";
 
-const Home = () =>{
-  return(
-    <div>
-      <NameSearch/>
-      <IdSearch/>
-      <h2>Reporting</h2>
-      <div id="reportingAreaName"></div>
-      <div id="reportingAreaId"></div>
-    </div>
-  );
-}
-
-export default Home; 
-
-// class Home extends React.Component{
-//   getName(){
-//     let name = document.querySelector('#nameIput');
-//     fetch("/api/pokemon/name" + name.value)
-//     .then((res) => {return res.json();})
-//     .then((processed) => {
-
-//       let resultElement = document.querySelector("#results");
-
-//       if(processed.error){
-//         resultElement.innerHTML = "Could not find!";
-//       }
-//       else{
-//         resultElement.innerHTML = "Its ID is" + processed.id;
-//       }
-//     });
-//   }
-
-//   render(){
-//     return(
-//       <div>
-//         <input type="text" id="nameInput" />
-//         <button onClick = {() => {this.getName()}}>SUBMIT</button>
-//         <div id="results"></div>
-//       </div>
-//     );
-//   }
-
-//   getId(){
-//     let id = document.querySelector("#idInput");
-
-//     fetch("/api/pokemon/id" + id.value)
-//     .then((res) => {return res.json();})
-//     .then((processed) => {
-//       let resultElement = document.querySelector("#results");
-
-//       if(processed.error){
-//         resultElement.innerHTML = "Could not find!";
-//       }
-//       else{
-//         resultElement.innerHTML = "Its ID is" + processed.id;
-//       }
-//     });
-//   }
-
-//   render(){
-//     return(
-//       <div>
-//         <input type="text" id="idInput" />
-//         <button onClick = {() => {this.getId()}}>SUBMIT</button>
-//         <div id="results"></div>
-//       </div>
-//     )
-//   }
-
+// const Home = () =>{
+//   return(
+//     <div>
+//       <NameSearch/>
+//       <IdSearch/>
+//       <h2>Reporting</h2>
+//       <div id="reportingAreaName"></div>
+//       <div id="reportingAreaId"></div>
+//     </div>
+//   );
 // }
 
-// export default Home;
+// export default Home; 
+
+class Home extends React.Component{
+  getName(){
+    let name = document.querySelector('#nameIput');
+    fetch("/api/pokemon/name/" + name.value)
+    .then((res) => {return res.json();})
+    .then((processed) => {
+
+      let resultElement = document.querySelector("#results");
+
+      if(processed.error){
+        resultElement.innerHTML = "Could not find!";
+      }
+      else{
+        resultElement.innerHTML = "Its ID is" + processed.id;
+      }
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <input type="text" id="nameInput" />
+        <button onClick={() => { this.getName() } }>SUBMIT</button>
+        <div id="results"></div>
+      </div>
+    );
+  }
+
+  // getId(){
+  //   let id = document.querySelector("#idInput");
+
+  //   fetch("/api/pokemon/id" + id.value)
+  //   .then((res) => {return res.json();})
+  //   .then((processed) => {
+  //     let resultElement = document.querySelector("#results");
+
+  //     if(processed.error){
+  //       resultElement.innerHTML = "Could not find!";
+  //     }
+  //     else{
+  //       resultElement.innerHTML = "Its ID is" + processed.id;
+  //     }
+  //   });
+  // }
+
+  // render(){
+  //   return(
+  //     <div>
+  //       <input type="text" id="idInput" />
+  //       <button onClick = {() => {this.getId()}}>SUBMIT</button>
+  //       <div id="results"></div>
+  //     </div>
+  //   )
+  // }
+
+}
+
+export default Home;
 
