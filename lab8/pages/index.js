8c7ee578a948eb0667a9a873feb8c59abc74bfed
leunk <leunk@knights.ucf.edu>
2020-03-19 18:02:50 -0400

    more more changes / not working

--- lab8/pages/index.js
+++ lab8/pages/index.js
@@ -43,32 +43,32 @@ class Home extends React.Component{
     );
   }
 
-  // getId(){
-  //   let id = document.querySelector("#idInput");
+  getId(){
+    let id = document.querySelector("#idInput");
 
-  //   fetch("/api/pokemon/id" + id.value)
-  //   .then((res) => {return res.json();})
-  //   .then((processed) => {
-  //     let resultElement = document.querySelector("#results");
+    fetch("/api/pokemon/id" + id.value)
+    .then((res) => {return res.json();})
+    .then((processed) => {
+      let resultElement = document.querySelector("#results");
 
-  //     if(processed.error){
-  //       resultElement.innerHTML = "Could not find!";
-  //     }
-  //     else{
-  //       resultElement.innerHTML = "Its ID is" + processed.id;
-  //     }
-  //   });
-  // }
+      if(processed.error){
+        resultElement.innerHTML = "Could not find!";
+      }
+      else{
+        resultElement.innerHTML = "Its ID is" + processed.id;
+      }
+    });
+  }
 
-  // render(){
-  //   return(
-  //     <div>
-  //       <input type="text" id="idInput" />
-  //       <button onClick = {() => {this.getId()}}>SUBMIT</button>
-  //       <div id="results"></div>
-  //     </div>
-  //   )
-  // }
+  render(){
+    return(
+      <div>
+        <input type="text" id="idInput" />
+        <button onClick = {() => {this.getId()}}>SUBMIT</button>
+        <div id="results"></div>
+      </div>
+    )
+  }
 
 }
 
