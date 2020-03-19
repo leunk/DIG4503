8f7f3f511b96787d4dac500715178633cdb1f493
leunk <leunk@knights.ucf.edu>
2020-03-19 17:34:54 -0400

    trying

--- lab8/pokemon/id/[id].js
+++ lab8/pokemon/id/[id].js
@@ -1,46 +1,64 @@
+//repl example Next API
+
 import getPokemon from 'json-pokemon/getPokemon';
-import React from "react";
-
-class IdSearch extends React.Component{
-    readId(event){
-        event.preventDefault();
-        let pokeId = document.querySelector("#pokemonId");
-
-        fetch("api/pokemon/id/" + pokeId.value).then((res)=>{
-            return res.json();
-        }).then((processed) =>{
-            let pokeResponse = document.querySelector("#reportingArea");
-            if(processed.error){
-                pokeResponse.innerHTML = processed.error;
-            }
-            else{
-                pokeResponse.innerHTML = processed.name;
-            }
-        });
-    }
-    render(){
-        return(
-            <div>
-                <form onSubmit = {this.readId}>
-                <p>Enter Pokemon ID:</p>
-                <input id = "pokemonId" type = "text" />
-                <button>Submit</button>
-                </form>
-            </div>
-        );
-    }
-}
 
 export default (req,res) => {
-    let result = {"error": "Whoops. Could not find id!"};
-
-    const id = parseInt(req.query.id);
-
-    let pokemon = getPokemon.getPokemonById(id);
-
-    if(pokemon !== null){
-        result = pokemon;
+        let result = {"error": "Whoops. Could not find id!"};
+    
+        const id = parseInt(req.query.id);
+    
+        let pokemon = getPokemon.getPokemonById(id);
+    
+        if(pokemon !== null){
+            result = pokemon;
+        }
+    
+        res.json(result);
     }
 
-    res.json(result);
-}
+
+// import React from "react";
+
+// class IdSearch extends React.Component{
+//     readId(event){
+//         event.preventDefault();
+//         let pokeId = document.querySelector("#pokemonId");
+
+//         fetch("api/pokemon/id/" + pokeId.value).then((res)=>{
+//             return res.json();
+//         }).then((processed) =>{
+//             let pokeResponse = document.querySelector("#reportingArea");
+//             if(processed.error){
+//                 pokeResponse.innerHTML = processed.error;
+//             }
+//             else{
+//                 pokeResponse.innerHTML = processed.name;
+//             }
+//         });
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <form onSubmit = {this.readId}>
+//                 <p>Enter Pokemon ID:</p>
+//                 <input id = "pokemonId" type = "text" />
+//                 <button>Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+
+// export default (req,res) => {
+//     let result = {"error": "Whoops. Could not find id!"};
+
+//     const id = parseInt(req.query.id);
+
+//     let pokemon = getPokemon.getPokemonById(id);
+
+//     if(pokemon !== null){
+//         result = pokemon;
+//     }
+
+//     res.json(result);
+// }
--- lab8/pokemon/name/[name].js
+++ lab8/pokemon/name/[name].js
@@ -1,38 +1,38 @@
 import getPokemon from 'json-pokemon/getPokemon';
-import React from 'react';
 
-class NameSearch extends React.Component {
 
-    readName(event) {
-        event.preventDefault();
-            let pokeName = document.querySelector("#pokemonName");
+// class NameSearch extends React.Component {
 
-            fetch("api/pokemon/name/" + pokeName.value).then((res) =>{
-                return res.json();
-            }).then((processed) => {
-                let pokeResponse = document.querySelector("#reportingArea");
-                if(processed.error){
-                    pokeResponse.innerHTML = processed.error;
-                }
-                else{
-                    pokeResponse.innerHTML = processed.name;
-                }
-            });
-        }
+//     readName(event) {
+//         event.preventDefault();
+//             let pokeName = document.querySelector("#pokemonName");
 
-    render() {
-        return(
-            <div>
-                <form onSubmit = {this.readName}>
-                <p>Enter Pokemon Name:</p>
-                <input id= "pokemonName" type="text" />
-                <button>Submit</button>
-                </form>
-            </div>
-        );
-    }
+//             fetch("api/pokemon/name/" + pokeName.value).then((res) =>{
+//                 return res.json();
+//             }).then((processed) => {
+//                 let pokeResponse = document.querySelector("#reportingArea");
+//                 if(processed.error){
+//                     pokeResponse.innerHTML = processed.error;
+//                 }
+//                 else{
+//                     pokeResponse.innerHTML = processed.name;
+//                 }
+//             });
+//         }
 
-}
+//     render() {
+//         return(
+//             <div>
+//                 <form onSubmit = {this.readName}>
+//                 <p>Enter Pokemon Name:</p>
+//                 <input id= "pokemonName" type="text" />
+//                 <button>Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+
+// }
 
 export default (req, res) => {
     let result = {"error": "Could not find name!"};
